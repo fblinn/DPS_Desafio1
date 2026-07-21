5030876ae8b2563b54fee5e95a7e0766c7dfdfa7
fblinn <149543268+fblinn@users.noreply.github.com>
2026-07-20 18:15:23 -0600

    oremos

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,5 +1,6 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+import { useDispatch, useSelector } from 'react-redux';
 
 // Hook tipado para despachar acciones al store de Redux
 export const useAppDispatch: () => AppDispatch = useDispatch;
